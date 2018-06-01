984030332d72684cd2e6a0d26cee239be07108ca
Patrick Knight <ptknight00@gmail.com>
2018-06-01 14:00:25 -0500

    navbar flex

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,16 +4,15 @@ import "./Navbar.css";
 const Navbar = (props) => {
     return (
         <nav className='navbar'>
-            <span className='navbar_left'>
-                <a href='/'>Home</a>                
-                <a href='/'>Home</a>                
-                <a href='/'>Home</a>                
-            </span>
-            <span className='navbar_right'>
-                <a href='/'>Home</a>                
-                <a href='/'>Home</a>                
-                <a href='/'>Home</a> 
-            </span>
+            <ul className='navbar_left' role='navigation'>
+                <li>Home</li>                
+                <li>Products</li>                
+                <li>Examples</li>                                
+            </ul>
+            <ul className='navbar_right' rolse='navigation'>               
+                <li>Cart</li>                
+                <li>Checkout</li> 
+            </ul>
         </nav>
     )
 };
