54815721384cdf5ae2633b8a5c35e3d7de9bd0ac
Patrick Knight <ptknight00@gmail.com>
2018-06-05 17:19:06 -0500

    navbar link routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import "./Navbar.css";
 
 class Navbar extends Component {
@@ -6,13 +7,39 @@ class Navbar extends Component {
         return (
             <nav className='navbar'>
                 <ul className='navbar_left' role='navigation'>
-                    <li className='navbar_home' name='home'><a href='#'>Home</a></li>                
-                    <li className='navbar_products' name='products'><a href='#'>Products</a></li>                
-                    <li className='navbar_examples' name='examples'><a href='#'>Examples</a></li>                                
+                    <li className='navbar_home' name='home'>
+                        <Link
+                            to='/'
+                            className={
+                                window.location.pathname === '/' ? 'nav-link active' : 'nav-link'
+                            }>
+                            Home
+                        </Link>
+                    </li>                
+                    <li className='navbar_products' name='products'>
+                        <Link
+                            to='products'
+                            className={
+                                window.location.pathname === '/products' ? 'nav-link active' : 'nav-link'
+                            }>
+                            Products
+                        </Link>
+                    </li>                
+                    <li className='navbar_examples' name='examples'>
+                        <a href='#'>Examples</a>
+                    </li>                                
                 </ul>
-                <ul className='navbar_right' rolse='navigation'>               
+                <ul className='navbar_right' role='navigation'>               
                     <li>Cart</li>                
-                    <li>Checkout</li> 
+                    <li>
+                        <Link
+                            to='/checkout'
+                            className={
+                                window.location.pathname === '/' ? 'nav-link active' : 'nav-link'
+                            }>
+                           Checkout 
+                        </Link>
+                    </li> 
                 </ul>
             </nav>
         )
