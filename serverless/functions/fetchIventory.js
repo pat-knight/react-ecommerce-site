5ba19ec631e10c88266bd43fa6547a1b2054fda4
Patrick Knight <ptknight00@gmail.com>
2018-06-06 18:31:46 -0500

    fetch products function

---
+++ serverless/functions/fetchIventory.js
@@ -0,0 +1,17 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports.handler = (event, context, callback) => {
+    return stripe.products.list({limit:10})
+        .then((products) => {
+            const response = {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+                body: JSON.stringify({
+                    data: products.data
+                }),
+            };
+            callback(null, response);
+        });
+}
